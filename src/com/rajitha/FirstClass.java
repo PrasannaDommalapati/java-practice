4c0eacc0c86166602ef0d842ce48d052064008e4
Rajitha <Rajitha@rajitha.com>
2018-08-21 21:22:24 +0100

    changes in the Java practice

--- src/com/rajitha/FirstClass.java
+++ src/com/rajitha/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Java Practice");
+        System.out.println("First DAY OF Automation");
 
     }
 }
