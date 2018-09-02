aad77b9ef1da0426317a8d906b8194f1e75e0817
Prasanna Kumar <prasanna.kumar@headforwards.com>
2018-09-02 20:22:23 +0100

    for example teaching

---
+++ src/com/rajitha/ForExample.java
@@ -0,0 +1,13 @@
+package com.rajitha;
+
+public class ForExample {
+
+    public static void main(String args[]) {
+        int i = 50;
+
+        for(;i >= 20;i--) {
+            System.out.println("value of i is :-"+i);
+
+        }
+    }
+}
